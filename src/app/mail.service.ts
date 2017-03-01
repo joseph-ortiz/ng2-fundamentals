d15458c81d24a0513f0fa3c769c93eba7408cb4d
jortiz <josepho011235@gmail.com>
2017-02-28 21:14:40 -0500

    passed values to cmpt with input

--- src/app/mail.service.ts
+++ src/app/mail.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class MailService {
-  message = "you've got mail!";
+  messages = [
+    "Woah",
+    "Excellent!",
+    "Totally Heinous"
+  ];
   constructor() { }
 
 }
--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-simple-form',
   template: `
     <p>
       simple-form Works!
+      {{message}}
       <input #myInput type="text">
       <button (click)="onClick($event, myInput)">Click me!</button>
     </p>
@@ -12,6 +13,8 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class SimpleFormComponent implements OnInit {
+
+  @Input() message;
 
   onClick(event, value){
     console.log('clicked', value);
